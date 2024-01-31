a72da32ce4ef55183d86e69c87448b13b9e0ec79
PoluektovaDarya <123874713+PoluektovaDarya@users.noreply.github.com>
2024-01-31 14:28:16 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,41 +1,42 @@
-// Источник напряжения 380 Вольт
-class Voltage380V {
-    public int provideVoltage() {
-        return 320;
+// Класс, работающий с напряжением 380 вольт
+class Voltage330Device {
+    public void provide330V() {
+        System.out.println("Предоставляет 330V");
     }
 }
 
-// Зарядное устройство 220 Вольт
-class Charger220V {
+// Класс, работающий с зарядным устройством 220 вольт
+class Voltage220Device {
     public void chargeWith220V() {
-        System.out.println("Заряжаем устройство от 220 Вольт");
+        System.out.println("Заряжается от 220V");
     }
 }
 
 // Адаптер для зарядного устройства
-class Adapter {
-    private Voltage380V voltage380V;
+class Adapter220vTo330v extends Voltage330Device {
+    private Voltage220Device voltage220Device;
 
-    public Adapter(Voltage380V voltage380V) {
-        this.voltage380V = voltage380V;
+    public Adapter220vTo330v(Voltage220Device voltage220Device) {
+        this.voltage220Device = voltage220Device;
     }
 
-    public void chargeWith220V() {
-        int voltage = voltage380V.provideVoltage();
-        if (voltage == 380) {
-            Charger220V charger220V = new Charger220V();
-            charger220V.chargeWith220V();
-        } else {
-            System.out.println("Неверное напряжение: " + voltage + " Вольт. Зарядка невозможна.");
-        }
+    @Override
+    public void provide330V() {
+        System.out.println("Адаптер преобразует напряжение...");
+        voltage220Device.chargeWith220V();
     }
 }
 
-public class Main {
+// Пример использования
+public class AdapterPatternExample {
     public static void main(String[] args) {
-        Voltage380V source380V = new Voltage380V();
-        Adapter adapter = new Adapter(source380V);
+        Voltage330Device voltage330Device = new Voltage330Device();
+        Voltage220Device voltage220Device = new Voltage220Device();
 
-        adapter.chargeWith220V();
+        Adapter220vTo330v adapter = new Adapter220vTo330v(voltage220Device);
+
+        // Используем адаптер для зарядки телефона через напряжение 330 вольт
+        voltage330Device.provide330V();
+        adapter.provide330V();
     }
 }
